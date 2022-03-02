5d63337bff75225af3d466fbe449ac2912ec735b
Marco <marco_camarlinghi@yahoo.it>
2022-03-02 11:17:20 +0100

    Modifica di gitignore, aggiunta alla repository della cartella cmake-build-debug, creazione file csv per salvare transazioni

---
+++ cmake-build-debug/CMakeFiles/transazioniFinanziarie.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/marco/Desktop/Laboratorio di Programmazione/codiceSerio/transazioniFinanziarie/acquisto.cpp" "C:/Users/marco/Desktop/Laboratorio di Programmazione/codiceSerio/transazioniFinanziarie/cmake-build-debug/CMakeFiles/transazioniFinanziarie.dir/acquisto.cpp.obj"
+  "C:/Users/marco/Desktop/Laboratorio di Programmazione/codiceSerio/transazioniFinanziarie/bonifico.cpp" "C:/Users/marco/Desktop/Laboratorio di Programmazione/codiceSerio/transazioniFinanziarie/cmake-build-debug/CMakeFiles/transazioniFinanziarie.dir/bonifico.cpp.obj"
+  "C:/Users/marco/Desktop/Laboratorio di Programmazione/codiceSerio/transazioniFinanziarie/main.cpp" "C:/Users/marco/Desktop/Laboratorio di Programmazione/codiceSerio/transazioniFinanziarie/cmake-build-debug/CMakeFiles/transazioniFinanziarie.dir/main.cpp.obj"
+  "C:/Users/marco/Desktop/Laboratorio di Programmazione/codiceSerio/transazioniFinanziarie/transazione.cpp" "C:/Users/marco/Desktop/Laboratorio di Programmazione/codiceSerio/transazioniFinanziarie/cmake-build-debug/CMakeFiles/transazioniFinanziarie.dir/transazione.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/transazioniFinanziarie.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transazioniFinanziarie.dir/acquisto.cpp.obj"
+  "CMakeFiles/transazioniFinanziarie.dir/bonifico.cpp.obj"
+  "CMakeFiles/transazioniFinanziarie.dir/main.cpp.obj"
+  "CMakeFiles/transazioniFinanziarie.dir/transazione.cpp.obj"
+  "libtransazioniFinanziarie.dll.a"
+  "transazioniFinanziarie.exe"
+  "transazioniFinanziarie.exe.manifest"
+  "transazioniFinanziarie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transazioniFinanziarie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
